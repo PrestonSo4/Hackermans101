27f117c47009bb36bd030e36e9cececc1915c379
prestonchso <prestonchso@gmail.com>
2021-03-26 11:16:44 -0700

    Commit

--- main.py
+++ main.py
@@ -3,4 +3,5 @@ from selenium import webdriver
 driver = webdriver.Chrome()
 driver.get('https://flipgrid.com/0d4b1f77')
 
-
+joinMS = driver.find_element_by_link_text('Join with Microsoft')
+joinMS.click()
