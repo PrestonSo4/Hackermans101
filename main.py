9305a2293a3a1a88c3948c44997ae671554b1d2f
prestonchso <prestonchso@gmail.com>
2021-03-27 12:26:30 -0700

    trying pyautogui

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
-import selenium
+import selenium, time, pyautogui, webbrowser
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from getpass import getpass
+startx, starty = pyautogui.position()
+email = input('Enter your email: ')
+psswrd = getpass('Enter your password: ')
 driver = webdriver.Chrome()
-driver.get('https://flipgrid.com/0d4b1f77')
-
-joinMS = driver.find_element_by_link_text('Join with Microsoft')
-joinMS.click()
+#driver.get('https://flipgrid.com/0d4b1f77')
+driver.get('https://login.microsoftonline.com/common/oauth2/v2.0/authorize?response_type=code&response_mode=query&client_id=f1143447-b07a-4557-b878-b78df8d45c13&redirect_uri=https://flipgrid.com/auth/sso&scope=User.Read&prompt=select_account&state=eyJyZWRpcmVjdF90byI6Ii8wZDRiMWY3Nz9yZW1lbWJlcj10cnVlIiwicHJvdmlkZXIiOiJtaWNyb3NvZnQiLCJyZW1lbWJlcl9tZSI6dHJ1ZSwidG9waWNfaWQiOjIxMTY1OTQwfQ==&sso_reload=true')
+email = "s-sop@bsd405.org"
+#joinMS = driver.find_element_by_xpath('//*[@id="app"]/div[2]/div/div/div[2]/div/div[1]/button[2]')
+#joinMS.click()
+time.sleep(2)
+loginMS = driver.find_element_by_name('loginfmt')
+loginMS.send_keys(email)
+nextMS = driver.find_element_by_id('idSIButton9')
+nextMS.click()
+psswrdMS = driver.find_element_by_id('i0118')
+psswrdMS.send_keys(psswrd)
+pyautogui.moveTo(633, 445, duration = 0, tween = pyautogui.easeInOutQuad)
+pyautogui.click()
+#driver.get('https://flipgrid.com/0d4b1f77')
