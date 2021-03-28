c8dfface98b9c206d5dcf09d95e8527e3b9ad1ad
prestonchso <prestonchso@gmail.com>
2021-03-28 09:38:58 -0700

    stuff

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-import selenium, time, pyautogui, webbrowser
+import selenium, time, pyautogui, webbrowser, random
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from getpass import getpass
-startx, starty = pyautogui.position()
-email = input('Enter your email: ')
-psswrd = getpass('Enter your password: ')
+email = 's-sop@bsd405.org'
+#email = input('Enter your email: ')= ''
+psswrd = input('Enter your password: ')
 driver = webdriver.Chrome()
 #driver.get('https://flipgrid.com/0d4b1f77')
 driver.get('https://login.microsoftonline.com/common/oauth2/v2.0/authorize?response_type=code&response_mode=query&client_id=f1143447-b07a-4557-b878-b78df8d45c13&redirect_uri=https://flipgrid.com/auth/sso&scope=User.Read&prompt=select_account&state=eyJyZWRpcmVjdF90byI6Ii8wZDRiMWY3Nz9yZW1lbWJlcj10cnVlIiwicHJvdmlkZXIiOiJtaWNyb3NvZnQiLCJyZW1lbWJlcl9tZSI6dHJ1ZSwidG9waWNfaWQiOjIxMTY1OTQwfQ==&sso_reload=true')
@@ -18,6 +18,20 @@ nextMS = driver.find_element_by_id('idSIButton9')
 nextMS.click()
 psswrdMS = driver.find_element_by_id('i0118')
 psswrdMS.send_keys(psswrd)
-pyautogui.moveTo(633, 445, duration = 0, tween = pyautogui.easeInOutQuad)
+pyautogui.moveTo(545, 652, duration=1, tween=pyautogui.easeInOutQuad)
+print('moved to!')
+time.sleep(1)
 pyautogui.click()
-#driver.get('https://flipgrid.com/0d4b1f77')
+time.sleep(1.5)
+pyautogui.click()
+print('clicked!')
+time.sleep(3)
+driver.get('https://flipgrid.com/0d4b1f77')
+move = pyautogui.moveTo(545, 932, duration=1, tween=pyautogui.easeInOutQuad)
+for i in range(10):
+    driver.get('https://flipgrid.com/0d4b1f77')
+    #time.sleep(0.08)
+    move if pyautogui.position() != (545, 932) else print()
+    pyautogui.click()
+    
+driver.close()
